9cfef41b4d78ff1696af566d0b1cfa92d2496c66
Jacopo <jack@manjaro.fritz.box>
2021-01-27 23:28:38 +0100

    added current user infos

--- Earth.py
+++ Earth.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from bs4 import BeautifulSoup
 
 class Earth(object):
 
@@ -11,6 +12,7 @@ class Earth(object):
         self.loggedCookies = self._load_cookies()
         self.s = requests.Session()
         self.s.cookies.set(**self.loggedCookies)
+        self.user_data = self._get_curr_user_infos()
 
     def _load_cookies(self, filename="./cookies.json"):
         with open(filename, 'r') as jsonCookies:
@@ -23,9 +25,24 @@ class Earth(object):
             data={"query":query}
         ).json()
 
+    def _get_curr_user_infos(self):
+        page = self.s.get("https://app.earth2.io/#profile")
+        soup = BeautifulSoup(page.content, "html.parser")
+        s = soup.findAll('script')
+        script_tag = s[-1:][0]
+        json_dict = str(script_tag).split("riot.auth0user = ")[1].split("static_url")[0].strip().replace("'", '"')
+        data = u'{}'.format(json_dict)
+        return json.loads(data)
+
+    def get_user_infos(self):
+        return self.user_data
+
     # Provo a comprare
     # tildIds => id celle che vogliamo acquistare
     def tryPayement(self, tileIds):
         query = 'mutation { buyNewLandfield( tiles: ' + tileIds + ', center: "55.014038 24.982635", description: "Dubai", location: "Dubai, Dubai, United Arab Emirates", promoCodeId: "undefined" ) { landfield { id, thumbnail, description, location, forSale, price, center, tileIndexes, owner { username }, transactionSet { price, timeStr, previousOwner { username, } owner { username, } } } } }'
         response = self._exec(query)
         return response
+
+e = Earth()
+e._get_curr_user_infos()
--- bot.py
+++ bot.py
@@ -3,12 +3,16 @@ from Earth import Earth
 
 if __name__ == "__main__":
     e = Earth()
+    user_infos = e.get_user_infos()
+    print(f"[!] Logged in as\nusername: {user_infos['username']}\nbalance: {user_infos['balance']}\nnetworth: {user_infos['networth']}\ntotalTiles: {user_infos['totalTiles']}\nspent: {user_infos['spent']}\n===========\n\n")
+    
     while True:
         print("[/] Trying to buy land ...")
-        output = e.tryPayement(str([35082115000621]))
+        output = e.tryPayement(str([152049601403821, 42098438626221]))
         if "errors" in output.keys():
             print("[-] Got Error \n" + str(output))
         else:
+            print(output)
             print("[+] Purchasing complete !")
-        # Each 6 minutes
-        time.sleep(360) 
+        # Each 2 minutes
+        time.sleep(120)
